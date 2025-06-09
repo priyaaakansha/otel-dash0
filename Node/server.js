f0a895e22591ec1858ecde46701e20f9c7d763a5
Aakansha Priya <aakanshapriya2000@gmail.com>
2025-06-09 23:29:23 +0530

    added logger for NodeJs

--- Node/server.js
+++ Node/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
+import winston from 'winston';
 
 const app = express();
 const port = 3000;
 
+// Basic logger setup
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(), 
+  transports: [new winston.transports.Console()],
+});
+
 app.use(express.json());
 
-// POST route
 app.post('/api/data', (req, res) => {
-    console.log('Received data from Java client:', req.body);
-    res.status(200).json({ message: 'Received successfully from Node js server' });
+  logger.info(`Received: ${JSON.stringify(req.body)}`);
+  res.status(200).json({ message: 'Received successfully from Node.js server' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+  logger.info(`Server running at http://localhost:${port}`);
 });
